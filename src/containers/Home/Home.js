96dec2411083701f90878187863a71d7a7847e57
Khallil Bailey <khallil.bailey@presidioidentity.com>
2020-11-18 14:49:46 -0800

    [add] button

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,12 +1,31 @@
-import React, { Component } from 'react'
-import HomeContainer from '../../styles/HomeContainer'
+import React, { Component } from 'react';
+import HomeContainer from '../../styles/HomeContainer';
+import Button from '../../styles/Button';
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: ''
+    }
+  }
+
+  componentDidMount() {
+    this.parseURL()
+  }
+
+  parseURL() {
+    let url = window.location.href;
+    console.log(url)
+  }
+
   render() {
     return(
       <HomeContainer>
         <h1>Client Middleware</h1>
-        
+        <Button>
+          <p>Log In with PI</p>
+        </Button>
       </HomeContainer>
     )
   }
---
+++ src/styles/Button.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Button = styled.div`
+    height: 2.5vh;
+    width: 200px;
+
+    background: #1D7D81;
+    border-radius: 3px 3px 3px 3px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+export default Button; 
